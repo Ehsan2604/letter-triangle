e327f76004e432a849b34f664c6fca0394e224a3
Ehsan2604 <ehsanullahehsan779@gmail.com>
2025-10-24 15:32:59 -0700

    Update letter-triangle.cpp

--- letter-triangle.cpp
+++ letter-triangle.cpp
@@ -1,6 +1,8 @@
 First run: 		for ( int row; row < N; row++ ) forgot to put the int row = 1 and i fixed it.
   second run: 		for ( char c = 'A'; c <= 'A'; row++ ) changed to for ( char c = 'A'; c <= 'A'; c++ )
   3rd run:  cout << endl; i had it connecte at first like this 			cout << c << " " << endl; but it was supposed to be seperated and it should have a bracket in between them.
+  4th run: for ( int row = 0; row < N; row++ ) forgot to add the bracket at the end {
+
 
 
 
